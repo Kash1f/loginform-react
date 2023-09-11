d8c02afd88bda825b53f49a5c31cc25a761c28f2
Kashif <ahmed.kashif1001@gmail.com>
2023-09-11 14:30:07 +0500

    comments

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
 
     const inputEvent = (event) => {
 
-      setName(event.target.value)
+      setName(event.target.value) //here event object will find the target in the event object and value in this case which will let us access what the user will enter
+
+      //and setName will change the state and this will become the current state as its the updated function
     }
 
   return (
