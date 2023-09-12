0618a18f745f3379d9f62bb05405ee92948ea304
Kashif <ahmed.kashif1001@gmail.com>
2023-09-12 15:57:43 +0500

    newfunction

--- src/App.js
+++ src/App.js
@@ -5,21 +5,30 @@ import { useState } from 'react';
 function App() {
 
   const [name, setName] = useState("");
+  const [fullName, setfullName] = useState("");
 
     const inputEvent = (event) => {
 
       setName(event.target.value) //here event object will find the target in the event object and value in this case which will let us access what the user will enter
-      
+
       //and setName will change the state and this will become the current state as its the updated function
     }
+
+    //here this onSubmit function will be called upon clicking the button which will then display the fullname
+    const onSubmit = () => {
+      setfullName(name)
+    }
+
+
 
   return (
     <>
     <div>
-    <h1>Hello {name}</h1>
-    <input type="text" placeholder="Enter Your Name" onChange={inputEvent}
+    <h1>Hello {fullName}</h1>
+    <input type="text" placeholder="Enter Your Name" onChange={inputEvent} value={name}/>
+    <button onClick={onSubmit}>Click Me</button>
     
-    /><button>Click Me</button>
+    
     </div>
     </>
   );
