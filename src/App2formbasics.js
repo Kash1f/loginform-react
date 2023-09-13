456d5a84079dad0639c450b39d789b7312328f3e
Kashif <ahmed.kashif1001@gmail.com>
2023-09-13 16:39:30 +0500

    loginpagewithnewfunc

--- src/App2formbasics.js
+++ src/App2formbasics.js
@@ -9,9 +9,12 @@ function App() {
 
     const inputEvent = (event) => {
 
-      setName(event.target.value) 
+      setName(event.target.value) //here event object will find the target in the event object and value in this case which will let us access what the user will enter
+
+      //and setName will change the state and this will become the current state as its the updated function
     }
 
+    //here this onSubmit function will be called upon clicking the button which will then display the fullName
     const onSubmit = () => {
       setfullName(name)
     }
